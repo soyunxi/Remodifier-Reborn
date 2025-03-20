2dd181fa473ad23afd72d4882c18f5cd7d8b977f
yunxi <3494035034@qq.com>
2025-03-20 23:46:44 +0800

    配置文件

---
+++ src/main/java/org/yunxi/remodifier/common/block/ReforgedTableBlock.java
@@ -0,0 +1,20 @@
+package org.yunxi.remodifier.common.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+public class ReforgedTableBlock extends Block implements EntityBlock {
+    public ReforgedTableBlock(Properties p_49795_) {
+        super(p_49795_);
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
+        return new ReforgedTableBlock(blockPos, blockState);
+    }
+}
